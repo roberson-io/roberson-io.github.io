d651becd3bca086d20394969bf0f417c93a702ab
Michael Roberson <michaelroberson@gmail.com>
2022-12-23 14:25:32 -0500

    Initial commit.

---
+++ static/js/scripts.js
@@ -0,0 +1,3 @@
+window.addEventListener('load', (event) => {
+  new cursoreffects.emojiCursor({emoji: ["⚖️", "🐄", "✨", "🐮", "🎵"]});
+});
---
+++ static/js/surprise.js
@@ -0,0 +1,5 @@
+const dangerButton = document.getElementById("danger-button");
+
+dangerButton.addEventListener("click", () => {
+    document.getElementById("airhorn").play();
+});
