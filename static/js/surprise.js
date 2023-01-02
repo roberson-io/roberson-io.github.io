b009b77a34d5772eb02358577fd46715efad42e5
Michael Roberson <michaelroberson@gmail.com>
2023-01-01 19:52:31 -0500

    New and improved danger button. Patreon link opens in new tab.

--- static/js/surprise.js
+++ static/js/surprise.js
@@ -1,5 +1,24 @@
 const dangerButton = document.getElementById("danger-button");
+const airhorn = document.getElementById("airhorn");
+const alaphama = document.getElementById("alaphama");
+const lilNose = document.getElementById("lil-nose");
+const recipe = document.getElementById("name-a-recipe");
+const niceFingernails = document.getElementById("nice-fingernails");
+const ghost = document.getElementById("swallowed-a-ghost");
+
+const sounds = [alaphama, lilNose, recipe, niceFingernails, ghost];
+
+Array.prototype.random = function () {
+    return this[Math.floor((Math.random()*this.length))];
+}
+
+let firstClick = true;
 
 dangerButton.addEventListener("click", () => {
-    document.getElementById("airhorn").play();
+    if(firstClick){
+        airhorn.play();
+        firstClick = false;
+    } else {
+        sounds.random().play();
+    }
 });
